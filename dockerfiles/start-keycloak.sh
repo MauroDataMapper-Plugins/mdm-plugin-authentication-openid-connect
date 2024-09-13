13d2fb8131c3255d805c9bdf96b1356ba85f3352
James Welch <james.welch@bdi.ox.ac.uk>
2024-09-13 11:09:50 +0100

    Update build script to start docker properly

--- dockerfiles/start-keycloak.sh
+++ dockerfiles/start-keycloak.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-docker run --rm \
+docker run --detach --rm \
 -p 8090:8080 \
 --name keycloak \
 keycloak-mdm:latest \
 start-dev --import-realm --proxy-headers xforwarded
+
+./wait-for-it.sh -t 0 localhost:8090
